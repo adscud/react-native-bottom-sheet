92d3ff39da1d0df37862d546e7ea9f8ade80dd50
ArthurDelbeke <arthurdelbeke@outlook.com>
2020-07-03 12:59:24 +0200

    improve bottom sheet

--- src/components/BottomSheetComponent.js
+++ src/components/BottomSheetComponent.js
@@ -1,79 +1,89 @@
-import React from 'react';
+import React, {
+    useState,
+    useRef,
+    useEffect,
+} from 'react'
 import {
     Animated,
-    Dimensions,
+    BackHandler,
     Keyboard,
     PanResponder,
-    Platform,
     ScrollView,
     StyleSheet,
     TouchableWithoutFeedback,
     View,
-    BackHandler
-} from 'react-native';
+    Dimensions,
+    Platform
+} from 'react-native'
 
-const {OS} = Platform;
-const {height} = Dimensions.get('window');
-const duration = 1000;
-const maxHeight = height * .9;
+const { height } = Dimensions.get('window')
+const { OS } = Platform
+const duration = 500
+const maxHeight = height * 0.9
 
-// Why use React.useRef with animated value : https://github.com/facebook/react-native/issues/25069
-const BottomSheetComponent = ({shouldOpen = false, onClose, renderContent, persistent = false}) => {
-    const [contentHeight, setContentHeight] = React.useState(0); // height of the content inside the bottom sheet
-    const [keyboardHeight, setKeyboardHeight] = React.useState(0); // get size of the keyboard
+// Why use useRef with animated value : https://github.com/facebook/react-native/issues/25069
+const BottomSheetComponent = ({ shouldOpen = false, onClose, renderContent, scrollEnabled = false, persistent = false }) => {
+    const [ isOpen, setIsOpen ] = useState(false)
+    const [ contentHeight, setContentHeight ] = useState(0) // height of the content inside the bottom sheet
+    const [ keyboardHeight, setKeyboardHeight ] = useState(0) // get size of the keyboard
+    const [ keyboardOpen, setKeyboardOpen ] = useState(false) // want to know if the keyboard is open or not
+    const [ animated, setAnimated ] = useState(false)
 
-    const opacity = React.useRef(new Animated.Value(0)).current; // manage the opacity of the background when bottom sheet is open
-    const translateY = React.useRef(new Animated.Value(height)).current; // manage position of the container's bottom sheet
-    const underKeyboardHeight = React.useRef(new Animated.Value(0)).current; // size of the view under the keyboard on ios to avoid keyboard over the content
-    const dragTopBackgroundHeight = new Animated.Value(0);
+    const opacity = useRef(new Animated.Value(0)).current // manage the opacity of the background when bottom sheet is open
+    const translateY = useRef(new Animated.Value(height)).current // manage position of the container's bottom sheet
+    const underKeyboardHeight = useRef(new Animated.Value(0)).current // size of the view under the keyboard on ios to avoid keyboard over the content
+    const dragTopBackgroundHeight = useRef(new Animated.Value(0)).current
 
-    const pan = new Animated.ValueXY(0); // { x: Number, y: Number }
+    const pan = new Animated.ValueXY(0) // { x: Number, y: Number }
 
-    const threshold = contentHeight * 0.1; // minimum height to drag to close the bottom sheet
+    const threshold = contentHeight * 0.1 // minimum height to drag to close the bottom sheet
 
-    React.useEffect(() => {
-        shouldOpen && openBottomSheet();
-    }, [shouldOpen]);
+    useEffect(() => {
+        if (shouldOpen && !isOpen) {
+            openBottomSheet()
+        } else if (!shouldOpen && isOpen) {
+            closeBottomSheet()
+        }
+    }, [ shouldOpen, isOpen ])
 
-    React.useEffect(() => {
-        keyboardHeight !== 0 ? keyboardShow() : keyboardDismiss()
+    useEffect(() => {
+        keyboardHeight !== 0 && !animated ? keyboardShow() : keyboardDismiss()
     }, [ keyboardHeight ])
 
-
-    OS === 'ios' && React.useEffect(() => {
+    useEffect(() => {
         const keyboardDidShowListener = Keyboard.addListener(
             'keyboardDidShow',
-            (event) => setKeyboardHeight(event.endCoordinates.height),
-        );
+            (event) => (OS === 'ios' && setKeyboardHeight(event.endCoordinates.height), setKeyboardOpen(true)),
+        )
         const keyboardDidHideListener = Keyboard.addListener(
             'keyboardDidHide',
-            () => setKeyboardHeight(0),
-        );
+            () => (OS === 'ios' && setKeyboardHeight(0), setKeyboardOpen(false)),
+        )
 
         return () => {
-            keyboardDidHideListener.remove();
-            keyboardDidShowListener.remove();
-        };
-    }, []);
+            keyboardDidHideListener.remove()
+            keyboardDidShowListener.remove()
+        }
+    }, [])
 
     // handle back button on android
-    OS === 'android' && React.useEffect(() => {
+    OS === 'android' && useEffect(() => {
         const backHandler = BackHandler.addEventListener(
-            "hardwareBackPress",
+            'hardwareBackPress',
             () => {
                 closeBottomSheet()
                 return true
-            }
-        );
+            },
+        )
 
-        return () => backHandler.remove();
+        return () => backHandler.remove()
     })
 
     // get the size of the content inside the bottom sheet
     const onLayout = (event) => {
-        const {height} = event.nativeEvent.layout;
-        setContentHeight(height);
-    };
+        const { height } = event.nativeEvent.layout
+        setContentHeight(height)
+    }
 
     const openBottomSheet = () => {
         Animated.parallel([
@@ -94,12 +104,11 @@ const BottomSheetComponent = ({shouldOpen = false, onClose, renderContent, persi
                     useNativeDriver: true,
                 },
             ),
-        ]).start();
-    };
+        ]).start(() => setIsOpen(true))
+    }
 
     // close the bottom sheet
     const closeBottomSheet = () => {
-        Keyboard.dismiss();
         Animated.parallel([
             Animated.timing(
                 opacity,
@@ -118,33 +127,38 @@ const BottomSheetComponent = ({shouldOpen = false, onClose, renderContent, persi
                 },
             ),
         ]).start(() => {
-            pan.setValue({x: 0, y: 0});
-            onClose();
-        });
-    };
+            pan.setValue({
+                x: 0,
+                y: 0,
+            })
+            onClose()
+            setIsOpen(false)
+        })
+    }
 
     const keyboardShow = () => {
+        setAnimated(true)
         Animated.timing(
             underKeyboardHeight,
             {
                 toValue: keyboardHeight,
-                duration: 500,
+                duration: 250,
                 useNativeDriver: false,
             },
-        ).start();
-    };
+        ).start(({ finished }) => finished && setAnimated(false))
+    }
 
     const keyboardDismiss = () => {
-        Keyboard.dismiss()
+        setAnimated(true)
         Animated.timing(
             underKeyboardHeight,
             {
                 toValue: 0,
-                duration: 500,
+                duration: 250,
                 useNativeDriver: false,
             },
-        ).start();
-    };
+        ).start(({ finished }) => finished && setAnimated(false))
+    }
 
     const panResponder = PanResponder.create({
         onMoveShouldSetPanResponderCapture: () => true,
@@ -160,22 +174,25 @@ const BottomSheetComponent = ({shouldOpen = false, onClose, renderContent, persi
             ],
             {
                 useNativeDriver: false, // Animated.event doesn't support the native driver
-                listener: (event, {dy}) => {
+                listener: (event, { dy }) => {
                     if (dy <= 0) { // if drag to the top, we grow up our view because don't want that the user seen nothing
-                        const dyAbs = Math.abs(dy); // ex : -20 => 20
-                        dragTopBackgroundHeight.setValue(dyAbs);
+                        const dyAbs = Math.abs(dy) // ex : -20 => 20
+                        dragTopBackgroundHeight.setValue(dyAbs)
                     }
                 },
             },
         ),
-        onPanResponderRelease: (e, {vx, vy}) => {
-            const {y} = pan;
+        onPanResponderRelease: (e, { vx, vy }) => {
+            const { y } = pan
             y.__getValue() > threshold && !persistent ? closeBottomSheet() : (
                 Animated.parallel([
                     Animated.spring( // go back to origin state
                         pan,
                         {
-                            toValue: {x: 0, y: 0},
+                            toValue: {
+                                x: 0,
+                                y: 0,
+                            },
                             useNativeDriver: false,
                         },
                     ),
@@ -187,11 +204,11 @@ const BottomSheetComponent = ({shouldOpen = false, onClose, renderContent, persi
                         },
                     ),
                 ]).start()
-            );
-            pan.flattenOffset();
+            )
+            pan.flattenOffset()
             // reset pan { x: 0, y: 0}
         },
-    });
+    })
 
     return (
         <>
@@ -201,13 +218,13 @@ const BottomSheetComponent = ({shouldOpen = false, onClose, renderContent, persi
                     {
                         transform: [
                             {
-                                translateY: translateY,
+                                translateY,
                             },
                         ],
                     },
                 ]}
             >
-                <View style={{flex: 1, position: 'relative'}}>
+                <View style={styles.parentContent}>
                     <TouchableWithoutFeedback
                         style={styles.layoutBottomSheet}
                         onPress={() => !persistent && closeBottomSheet()}
@@ -235,44 +252,43 @@ const BottomSheetComponent = ({shouldOpen = false, onClose, renderContent, persi
                         ]}
                         onLayout={onLayout}
                     >
-                        <View
-                            {...panResponder.panHandlers}
-                            style={styles.anchorView}
+                        {!persistent && (
+                            <View
+                                {...panResponder.panHandlers}
+                                style={styles.anchorView}
+                            >
+                                <View style={styles.anchor} />
+                            </View>
+                        )}
+                        <ScrollView
+                            scrollEnabled={scrollEnabled || keyboardOpen}
+                            keyboardShouldPersistTaps='always'
                         >
-                            <View style={styles.anchor}/>
-                        </View>
-                        <TouchableWithoutFeedback onPress={keyboardDismiss}>
-                            {contentHeight <= maxHeight ? (
-                                <View>
-                                    {renderContent()}
-                                    {OS === 'ios' && (
-                                        <Animated.View style={{ height: underKeyboardHeight }}/>
-                                    )}
-                                </View>
-                            ) : ( // content > maxHeight so we need to scroll inside our bottom sheet
-                                <ScrollView>
-                                    {renderContent()}
-                                    {OS === 'ios' && (
-                                        <Animated.View style={{ height: underKeyboardHeight }}/>
-                                    )}
-                                </ScrollView>
+                            {renderContent()}
+                            {OS === 'ios' && (
+                                <Animated.View style={{ height: underKeyboardHeight }} />
                             )}
-                        </TouchableWithoutFeedback>
+                        </ScrollView>
                     </Animated.View>
                     <Animated.View
-                        style={[styles.dragTopBackground, {height: dragTopBackgroundHeight}]}
+                        style={[ styles.dragTopBackground, { height: dragTopBackgroundHeight } ]}
                     />
                 </View>
             </Animated.View>
         </>
-    );
-};
+    )
+}
 
 const styles = StyleSheet.create({
     containerBottomSheet: {
+        zIndex: 35,
         position: 'absolute',
         height: '100%',
         width: '100%',
+    },
+    parentContent: {
+        flex: 1,
+        position: 'relative',
     },
     layoutBottomSheet: {
         height: height,
@@ -281,7 +297,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0, 0, 0, 0.15)',
     },
     bottomSheet: {
-        maxHeight: height * 0.9,
+        maxHeight: maxHeight,
         width: '100%',
         position: 'absolute',
         bottom: 0,
@@ -310,6 +326,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#ffffff',
         width: '100%',
     },
-});
+})
 
-export default BottomSheetComponent;
+export default BottomSheetComponent
